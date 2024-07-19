1ad2f5306f6fee37b885e6daef6d6019721ade55
Gonzalo Monar <gonza.monar@gmail.com>
2024-07-19 00:42:04 -0300

    Added the empty services and pages to the project.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { CreateComponent } from './pages/create/create.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { DeleteComponent } from './pages/delete/delete.component';
+import { ReadComponent } from './pages/read/read.component';
+import { StorageComponent } from './pages/storage/storage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
+    },
+    {
+        path: 'create',
+        component: CreateComponent
+    },
+    {
+        path: 'read',
+        component: ReadComponent
+    },
+    {
+        path: 'update',
+        component: UpdateComponent
+    },
+    {
+        path: 'delete',
+        component: DeleteComponent
+    },
+    {
+        path: 'storage',
+        component: StorageComponent
+    },
+];
